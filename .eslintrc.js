9662f40339f912409e8e07662ad10d398baffa7f
Asad Hazara <41273267+asadhazara@users.noreply.github.com>
2020-07-07 11:49:49 +0200

    fix: 🐛 add ability to add explicit any type

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "react/prop-types": "off",
     "import/order": [
       "error", {
